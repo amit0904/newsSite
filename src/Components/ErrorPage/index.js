542760c1b31032583e77a0d9eb96d8b4d2be0d5c
Amit Sharma <sharma.amit0904@gmail.com>
2020-08-13 02:01:15 +1000

    fix error message's position

--- src/Components/ErrorPage/index.js
+++ src/Components/ErrorPage/index.js
@@ -5,7 +5,7 @@ import { faExclamationTriangle} from '@fortawesome/free-solid-svg-icons'
 
 function ErrorPage(props){
     return(        
-        <div style={{textAlign: "center", marginTop: "300px", position : "absolute", zIndex : "-1"}}>
+        <div style={{textAlign: "center", marginTop: "300px"}}>
             <FontAwesomeIcon icon={faExclamationTriangle} size="2x" color="red" />
             <h3 style={{color : "white", display : "inline-block", marginLeft : "10px"}}>{props.message}</h3>
         </div>
